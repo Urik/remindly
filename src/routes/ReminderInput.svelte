936d91a7a5fd7b58739b4262eeff5e42267a5dad
Uri Berman Kleiner <uriberman@gmail.com>
2024-05-20 11:29:27 -0400

    added max length to input field

--- src/routes/ReminderInput.svelte
+++ src/routes/ReminderInput.svelte
@@ -1,5 +1,9 @@
 <script lang="ts">
   export let inputContent: string;
+  export let maxLength: number = 500;
 </script>
 
-<input type="text" placeholder="what do you want to remember?" class="nes-input" bind:value={inputContent} on:input >
+<div class="flex items-center">
+  <input type="text" maxlength={maxLength} placeholder="what do you want to remember?" class="nes-input grow" bind:value={inputContent} on:input >
+  <span class="ml-4">{inputContent.length}/{maxLength}</span>
+</div>
