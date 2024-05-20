a2f7290186b56a314c206d6e82a9a0b483c10424
Uri Berman Kleiner <uriberman@gmail.com>
2024-05-20 11:38:38 -0400

    added words counter and max input length

--- src/routes/ReminderInput.svelte
+++ src/routes/ReminderInput.svelte
@@ -3,7 +3,7 @@
   export let maxLength: number = 500;
 </script>
 
-<div class="flex items-center">
+<div class="md:flex md:items-center">
   <input type="text" maxlength={maxLength} placeholder="what do you want to remember?" class="nes-input grow" bind:value={inputContent} on:input >
-  <span class="ml-4">{inputContent.length}/{maxLength}</span>
+  <div class="mt-2 md:ml-4 md:mt-0">{inputContent.length}/{maxLength}</div>
 </div>
